0abb7aa2af246943591387396c2a83b54ff74ddf
henryyc <henryyc@gmail.com>
2017-10-23 16:13:04 -0400

    hmm

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var calendar;
+var listings;
+var neighbourhoods;
+var reviews;
+
+var request = require('request');
+request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/calendar.csv', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        calendar = body;
+    }
+});
+
+request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/calendar.csv', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        listings = body;
+    }
+});
+
+request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/calendar.csv', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        neighbourhoods = body;
+    }
+});
+
+request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/calendar.csv', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        reviews = body;
+    }
+});
--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,8 +1,1 @@
-var client = new XMLHttpRequest();
-client.open('GET', 'https://crossorigin.me/https://github.com/henryyc/air-list/tree/master/data/reviews.csv');
-var reviews;
-client.onreadystatechange = function() {
-  reviews = client.responseText;
-}
-client.send();
-alert(reviews.length);
+
