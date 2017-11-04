22cc0a1bcc95b2d98517460f1ea14b3893c87833
henryyc <henryyc@gmail.com>
2017-11-04 02:36:19 -0400

    Revert "Revert "temp""
    This reverts commit 0620f43d221cada41b4d08cae05652cdc0a08342.

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -21,6 +21,148 @@ module.exports = function() {
     console.log("initial maps created");
   }
 
+  //create interactive graph
+  this.graphPrices = function(horiAxis, priceData, priceFreq) {
+
+    var freq = new Array(horiAxis.length);
+    for (var i = 0; i < freq.length; i++){
+       freq[i] = priceData[i] ;/// priceFreq[i];
+
+      //if(priceFreq[i] == 0)
+        //console.log("lmao oops");
+      //freq[i] = 4;
+    }
+
+    /* from my old code: https://github.com/usnistgov/PasswordVizTool/blob/master/Histogram/histogram.html */
+    var margin = {
+        top: 40,
+        right: 20,
+        bottom: 30,
+        left: 40
+      },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+    var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+    var y = d3.scale.linear()
+      .range([height, 0]);
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        /* CUST9: Change tooltip content & color (for color: span style='color:yourcolor'>)*/
+        return d.letter + "  <strong>Frequency:<\/strong> <span style='color:red'>" + d.frequency + "<\/span>";
+      })
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    svg.call(tip);
+
+    // The new data variable.
+    var data = [];
+    for (var i = 0; i < horiAxis.length; i++)
+      data.push({
+        letter: horiAxis[i],
+        frequency: freq[i],
+        order: i
+      });
+
+    function type(d) {
+      d.frequency = +d.frequency;
+      return d;
+    }
+    x.domain(data.map(function(d) {
+      return d.letter;
+    }));
+    y.domain([0, d3.max(data, function(d) {
+      return d.frequency;
+    })]);
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+    svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) {
+        return x(d.letter);
+      })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) {
+        return y(d.frequency);
+      })
+      .attr("height", function(d) {
+        return height - y(d.frequency);
+      })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+    d3.selectAll("sortIt").on("change", change);
+    var sortTimeout = setTimeout(function() {
+      d3.selectAll("sortIt").property("checked", true).each(change);
+    }, 2000);
+    //sort
+    function change() {
+      clearTimeout(sortTimeout);
+      // Copy-on-write since tweens are evaluated after a delay.
+      var x0 = x.domain(data.sort(this.checked
+            /* CUST10: Change graph sort */
+            /* Use this: ? function(a, b) { return a.frequency - b.frequency; } to sort ascending */
+            ?
+            function(a, b) {
+              return b.frequency - a.frequency;
+            }
+            /* Use this: : function(a, b) { return d3.ascending(b.order, a.order); }) to sort from space to A*/
+            :
+            function(a, b) {
+              return d3.ascending(a.order, b.order);
+            })
+
+          .map(function(d) {
+            return d.letter;
+          }))
+        .copy();
+      svg.selectAll(".bar")
+        /* Use this: .sort(function(a, b) { return x0(b.letter) - x0(a.letter); }); to sort smaller values first*/
+        .sort(function(a, b) {
+          return x0(a.letter) - x0(b.letter);
+        });
+      var transition = svg.transition().duration(750),
+        delay = function(d, i) {
+          return i * 50;
+        };
+      transition.selectAll(".bar")
+        .delay(delay)
+        .attr("x", function(d) {
+          return x0(d.letter);
+        });
+      transition.select(".x.axis")
+        .call(xAxis)
+        .selectAll("g")
+        .delay(delay);
+    }
+
+    console.log("cost graph created");
+  }
+
   //create heatmap layer for cost map
   this.addHeat = function(lat, long, price, heatmapData, complete) {
 
@@ -33,34 +175,55 @@ module.exports = function() {
 
       heatmap.setMap(costMap);
       console.log("heat costs layered");
-      document.getElementById('loadPercent').innerHTML = 'Press &#39Learn more&#39 to get started.' ;
+      document.getElementById('loadPercent').innerHTML = 'Press &#39Learn more&#39 to get started.';
     }
 
     //continue adding to dataset
     else {
-      //FIX WEIGHT bruh
-      //var weight = new google.maps.visualization.WeightedLocation(new google.maps.LatLng(lat, long), +price);
-      var weight = new google.maps.LatLng(lat, long);
+      var weight = {
+        location: new google.maps.LatLng(lat, long),
+        weight: parseFloat(price)
+      }; //check up later
       heatmapData.push(weight);
     }
   }
 
   //add a marker to listing map
-  this.addMarker = function(lat, long) {
+  this.addMarker = function(data) {
 
-    //console.log(listing);
-    geocoder = new google.maps.Geocoder();
+    var contentString = '<div id="content">' +
+      '<div id="siteNotice">' +
+      '</div>' +
+      '<h1 id="firstHeading" class="firstHeading" style="color:black">Uluru</h1>' +
+      '<div id="bodyContent">' +
 
-    var contentString = "banana";
+      '<p style="color:black"> <b style="color:black">Uluru</b>, also referred to as <b style="color:black">Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the ' +
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+      'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+      'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+      'Aboriginal people of the area. It has many springs, waterholes, ' +
+      'rock caves and ancient paintings. Uluru is listed as a World ' +
+      'Heritage Site.' +
+
+      '<br />More info: ' + data["host_url"] + '</p>' +
+
+      '</div>' +
+      '</div>';
+
+
     var infowindow = new google.maps.InfoWindow({
-      content: contentString//this shit dont work
+      content: '<p style="color:black;">' + contentString + '</p>'
     });
 
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, long),
+      position: new google.maps.LatLng(data["latitude"], data["longitude"]),
       map: listingsMap,
-      title: 'Fruit'
+      title: data["host_name"]
     });
+
     marker.addListener('click', function() {
       infowindow.open(listingsMap, marker);
     });
--- public/js/read.js
+++ public/js/read.js
@@ -13,22 +13,58 @@ require('./jquery.csv.js');
 request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/calendar.csv', function(error, response, body) {
   if (!error && response.statusCode == 200) {
     calendar = body;
-    lists(calendar);
+    neighbours(calendar);
   }
 });
 
-function lists(calendar) {
+function neighbours(calendar) {
+  request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/neighbourhoods.csv', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      neighbourhoods = body;
+
+      var csv = require("fast-csv");
+      var heatmapData = [];
+
+      var CSV_STRING = body;
+      var xAxis = [];
+      var i = 0;
+
+      csv
+        .fromString(CSV_STRING, {
+          headers: true
+        })
+        .on("data", function(data) {
+          xAxis.push(data["neighbourhood"]);
+          xAxis[i] = [];
+          i++;
+        })
+        .on("end", function() {
+
+          console.log("calendar data finished being sent");
+        });
+
+      lists(calendar, xAxis, neighbourhoods);
+    }
+  });
+}
+
+function lists(calendar, xAxis, neighbourhoods) {
   request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/listings.csv', function(error, response, body) {
     if (!error && response.statusCode == 200) {
       listings = body;
 
-      //var GoogleMapsAPI = require('googlemaps');
       require('./graphs.js')();
       initMap();
 
       //go through the csv line by line to graph the markers one by one
       var csv = require("fast-csv");
       var heatmapData = [];
+      var priceData = new Array(xAxis.length); //keep track of price for each neighbourhood
+      var priceFreq = new Array(xAxis.length); //keep track of number of listings in each neighbourhood
+      for(var i = 0; i < priceData.length; i++){
+        priceData[i] = 0;
+        priceFreq[i] = 0;
+      }
 
       var CSV_STRING = body;
 
@@ -39,28 +75,29 @@ function lists(calendar) {
         .on("data", function(data) {
           var lat = data["latitude"];
           var long = data["longitude"];
-          var price = data["price"];
-        /*  var weight = google.maps.visualization.WeightedLocation(new google.maps.LatLng(lat, long), price);
-          heatmapData.push(weight);*/
+          var price = parseFloat(data["price"].substring(1));
+          console.log(price);
+
+          //add to basic price statistics
+          var neighbourhood = data["host_neighbourhood"];
+          for(var i = 0; i < xAxis.length; i++){
+            if(xAxis[i] == neighbourhood){
+              //substr by 1 to get rid of dollar sign
+              priceData[i] += price;
+              priceFreq[i]++;
+              i = xAxis.length;
+            }
+          }
 
           addHeat(lat, long, price, heatmapData, false)
-          addMarker(lat, long)
+          addMarker(data)
         })
         .on("end", function() {
-          //addHeat(heatmapData);
+          graphPrices(xAxis, priceData, priceFreq);
           addHeat(0, 0, 0, heatmapData, true);
-          console.log("listing data finished being sent");
+          console.log("listing and price data finished being sent");
         });
 
-      neighbours(calendar, listings);
-    }
-  });
-}
-
-function neighbours(calendar, listings) {
-  request.get('https://raw.githubusercontent.com/henryyc/air-list/master/data/neighbourhoods.csv', function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      neighbourhoods = body;
       finalFile(calendar, listings, neighbourhoods);
     }
   });
